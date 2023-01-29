11c5bfa6a0677c0ac40c1ef95fb653bb938a142e
jhdark <james.dark.95@gmail.com>
2023-01-29 09:08:09 +0000

    more optimised times

--- benchmark.py
+++ benchmark.py
@@ -6,19 +6,16 @@ for Ha_value in test_values:
 
     if Ha_value == 0:
         total_time = 0.5
-        dt = 1 / 200
     elif Ha_value == 10:
-        total_time = 0.1
-        dt = 1 / 1000
+        total_time = 0.5
     elif Ha_value == 30:
-        total_time = 0.05
-        dt = 1 / 2000
+        total_time = 0.1
     elif Ha_value == 60:
         total_time = 0.02
-        dt = 1 / 5000
     elif Ha_value == 100:
         total_time = 0.01
-        dt = 1 / 10000
+
+    dt = total_time / 100
 
     print("Running case Hartmann_no = {}, conductive".format(Ha_value))
     conductive_results_folder = "Results/fully_conductive/Ha={}/".format(Ha_value)
