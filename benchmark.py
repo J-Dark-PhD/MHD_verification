cd965b09a86c77d34c5aca7d20164878c842e371
jhdark <james.dark.95@gmail.com>
2023-02-11 11:13:37 +0000

    refactored

--- benchmark.py
+++ benchmark.py
@@ -1,29 +1,13 @@
 from mhd import mhd_sim
 
 Ha_test_values = [0, 10, 30, 60, 100]
+conductive_total_times = [5e-01, 5e-01, 1e-01, 2e-02, 1e-02]
+conductive_dts = [5e-03, 5e-03, 1e-03, 2e-04, 1e-04]
+conductive_mesh_densities = [30, 30, 40, 100, 200]
 
-for Ha_value in Ha_test_values:
-
-    if Ha_value == 0:
-        total_time = 5e-01
-        dt = 5e-03
-        mesh_density = 30
-    elif Ha_value == 10:
-        total_time = 5e-01
-        dt = 5e-03
-        mesh_density = 30
-    elif Ha_value == 30:
-        total_time = 1e-01
-        dt = 1e-03
-        mesh_density = 40
-    elif Ha_value == 60:
-        total_time = 2e-02
-        dt = 2e-04
-        mesh_density = 100
-    elif Ha_value == 100:
-        total_time = 1e-02
-        dt = 1e-04
-        mesh_density = 200
+for Ha_value, total_time, dt, mesh_density in zip(
+    Ha_test_values, conductive_total_times, conductive_dts, conductive_mesh_densities
+):
 
     print("Running case Hartmann_no = {}, conductive".format(Ha_value))
     conductive_results_folder = "Results/fully_conductive/Ha={}/".format(Ha_value)
@@ -38,28 +22,13 @@ for Ha_value in Ha_test_values:
         Nz=mesh_density,
     )
 
-for Ha_value in Ha_test_values:
+insulated_total_times = [5e-01, 5e-01, 1e-01, 4e-02, 3e-02]
+insulated_dts = [5e-03, 5e-03, 1e-03, 2e-04, 1e-04]
+insulated_mesh_densities = [30, 40, 100, 100, 100]
 
-    if Ha_value == 0:
-        total_time = 5e-01
-        dt = 5e-03
-        mesh_density = 30
-    elif Ha_value == 10:
-        total_time = 5e-01
-        dt = 5e-03
-        mesh_density = 40
-    elif Ha_value == 30:
-        total_time = 1e-01
-        dt = 1e-03
-        mesh_density = 100
-    elif Ha_value == 60:
-        total_time = 4e-02
-        dt = 2e-04
-        mesh_density = 100
-    elif Ha_value == 100:
-        total_time = 3e-02
-        dt = 1e-04
-        mesh_density = 100
+for Ha_value, total_time, dt, mesh_density in zip(
+    Ha_test_values, insulated_total_times, insulated_dts, insulated_mesh_densities
+):
 
     print("Running case Hartmann_no = {}, insulated".format(Ha_value))
     insulated_results_folder = "Results/fully_insulated/Ha={}/".format(Ha_value)
