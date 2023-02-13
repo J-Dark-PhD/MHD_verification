d3d29a4ec5a8be3057d383ebb89e580c6356db9e
James laptop <james.dark.95@gmail.com>
2023-02-13 09:38:15 +0000

    updated format

--- mesh_testing.py
+++ mesh_testing.py
@@ -1,26 +1,16 @@
 from mhd import mhd_sim
 
-Ha_test_values = [0, 10, 30, 60]
 mesh_test_values = [20, 30, 40, 50]
 
-for Ha_value in Ha_test_values:
+Ha_test_values = [0, 10, 30, 60, 100]
+conductive_total_times = [5e-01, 5e-01, 1e-01, 2e-02, 1e-02]
+conductive_dts = [5e-03, 5e-03, 1e-03, 2e-04, 1e-04]
+
+for Ha_value, total_time, dt in zip(
+    Ha_test_values, conductive_total_times, conductive_dts
+):
     for mesh_value in mesh_test_values:
-
-        N_cells = mesh_value
-
-        if Ha_value == 0:
-            total_time = 5e-01
-            dt = 5e-03
-        elif Ha_value == 10:
-            total_time = 5e-01
-            dt = 5e-03
-        elif Ha_value == 30:
-            total_time = 1e-01
-            dt = 1e-03
-        elif Ha_value == 60:
-            total_time = 4e-02
-            dt = 2e-04
-
+    
         print("Running case Hartmann_no = {}, conductive".format(Ha_value))
         print("Mesh size = {} x {}".format(mesh_value, mesh_value))
         conductive_results_folder = (
@@ -35,10 +25,18 @@ for Ha_value in Ha_test_values:
             total_time=total_time,
             dt=dt,
             Nx=20,
-            Ny=N_cells,
-            Nz=N_cells,
+            Ny=mesh_value,
+            Nz=mesh_value,
             export_mode=2,
         )
+
+insulated_total_times = [5e-01, 5e-01, 1e-01, 4e-02, 3e-02]
+insulated_dts = [5e-03, 5e-03, 1e-03, 2e-04, 1e-04]
+
+for Ha_value, total_time, dt in zip(
+    Ha_test_values, insulated_total_times, insulated_dts
+):
+    for mesh_value in mesh_test_values:
 
         print("Running case Hartmann_no = {}, insulated".format(Ha_value))
         print("Mesh size = {} x {}".format(mesh_value, mesh_value))
@@ -54,7 +52,8 @@ for Ha_value in Ha_test_values:
             total_time=total_time,
             dt=dt,
             Nx=20,
-            Ny=N_cells,
-            Nz=N_cells,
+            Ny=mesh_value,
+            Nz=mesh_value,
             export_mode=2,
         )
+
